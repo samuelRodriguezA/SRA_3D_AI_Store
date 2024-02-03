67fcf09f9bbfc8cacba2ea50185e7ee47043295d
samuelRodriguezA <srodriguezarmesto@gmail.com>
2024-02-03 14:06:01 -0500

    client fix

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -14,7 +14,6 @@ const CanvasModel = () => {
       className="w-full max-w-full h-full transition-all ease-in"
     >
       <ambientLight intensity={0.5} />
-      <Environment preset="city" />
 
       <CameraRig>
         <Backdrop />
